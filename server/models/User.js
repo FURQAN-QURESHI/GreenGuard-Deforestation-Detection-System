7702f4a790f4b006b6a540b996f26c1f222b2803
FURQAN-QURESHI <Furqanqureshi247@gmail.com>
2025-08-29 23:18:52 +0500

    User schema with bcrypt

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password_hash: {
+        type: String,
+        required: true
+    },
+    organization: {
+        type: String,
+        default: 'Individual'
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    date_created: {
+        type: Date,
+        default: Date.now
+    },
+    otp: {
+        type: String
+    },
+    otpExpires: {
+        type: Date
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
+// feat(auth): migrate User schema to name/password_hash fields
